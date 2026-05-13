0afcf3128e8e3c94b8a1320b13625a63fe68f6e3
Rishav Anand <tominati6@gmail.com>
2026-05-13 04:49:17 +0100

    Clearing everything to learn from Krish Niak

--- Python/hello.py
+++
@@ -1,3 +0,0 @@
-# This is comment in python that the interpreter ignores
-print("Hello World!")
-print("Print function is used to print on the console")
--- Python/user_input.py
+++
@@ -1,9 +0,0 @@
-take_input = input("A message to show in the console")
-
-print(take_input)
-
-age = input("Enter your age ") # it takes in string format
-print(age)
-
-# so we can type cast input functions to int
-age_as_int = int(input("Enter your age : "))
